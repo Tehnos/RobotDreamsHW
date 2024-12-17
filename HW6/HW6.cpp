5346ddfa7f9145c3819bc0246d52cf524d489997
Tehnos <tehnos2013@gmail.com>
2024-12-17 23:56:20 +0200

    homework done

--- HW6/HW6.cpp
+++ HW6/HW6.cpp
@@ -5,52 +5,52 @@ int main()
 {
 	int a1, d, n;
 
-	//std::cout << "Enter first element(a1):\n";
-	//std::cin >> a1;	
-	//std::cout << "Enter arithmetic progression step(d):\n";
-	//std::cin >> d;
-	//std::cout << "Enter the number of the last element of the arithmetic progression(n):\n";
-	//std::cin >> n;
-	//std::cout << "Your arimetic progression with:\n-initial element " << a1 << "\n-progression step " << d << "\n-number of elements " << n<<std::endl << std::endl;
-	//for (int i = 0; i < n; i++)
-	//{
-	//	int element = a1 + i * d;
-	//	std::cout << element << " ";
-	//}
-	//std::cout << std::endl;
+	std::cout << "Enter first element(a1):\n";
+	std::cin >> a1;	
+	std::cout << "Enter arithmetic progression step(d):\n";
+	std::cin >> d;
+	std::cout << "Enter the number of the last element of the arithmetic progression(n):\n";
+	std::cin >> n;
+	std::cout << "Your arimetic progression with:\n-initial element " << a1 << "\n-progression step " << d << "\n-number of elements " << n<<std::endl << std::endl;
+	for (int i = 0; i < n; i++)
+	{
+		int element = a1 + i * d;
+		std::cout << element << " ";
+	}
+	std::cout << std::endl;
 
 
-	//std::cout << "Enter weight:\n";
-	//std::cin >> a1;
-	//std::cout << "Enter height:\n";
-	//std::cin >> d;
+	std::cout << "Enter weight:\n";
+	std::cin >> a1;
+	std::cout << "Enter height:\n";
+	std::cin >> d;
 
-	//for (int i = 0; i < a1; i++)
-	//{
-	//	for (int j = i; j < d; j++)
-	//	{
-	//		std::cout << " * ";
-	//	}
-	//	std::cout << std::endl;
-	//}
-	//	std::cout << std::endl;
-	//for (int i = 0; i < a1; i++)
-	//{
-	//	for (int j = 0; j <= i; j++)
-	//	{
-	//		std::cout << " * ";
-	//	}
-	//	std::cout << std::endl;
-	//}		
-	//std::cout << std::endl;
-	//for (int i = 0; i < a1; i++)
-	//{
-	//	for (int j = 0; j < d; j++)
-	//	{
-	//		std::cout << " * ";
-	//	}
-	//	std::cout << std::endl;
-	//}
+	for (int i = 0; i < a1; i++)
+	{
+		for (int j = i; j < d; j++)
+		{
+			std::cout << " * ";
+		}
+		std::cout << std::endl;
+	}
+		std::cout << std::endl;
+	for (int i = 0; i < a1; i++)
+	{
+		for (int j = 0; j <= i; j++)
+		{
+			std::cout << " * ";
+		}
+		std::cout << std::endl;
+	}		
+	std::cout << std::endl;
+	for (int i = 0; i < a1; i++)
+	{
+		for (int j = 0; j < d; j++)
+		{
+			std::cout << " * ";
+		}
+		std::cout << std::endl;
+	}
 	/*std::cout << std::endl;
 
 	char ch;
@@ -82,17 +82,17 @@ int main()
 			std::cout << ch<<"\n";
 
 	}*/
-	//std::cout << std::endl;
-	//int a = 0, b = 1;
-	//std::cout << "Enter the number of the Fibonacci sequence element: ";
-	//std::cin >> n;
-	//for (int i = 2; i <= n; ++i)
-	//{
-	//	int fib_n = a + b;
-	//	a = b;
-	//	b = fib_n;
-	//	std::cout << fib_n <<" ";
-	//}
+	std::cout << std::endl;
+	int a = 0, b = 1;
+	std::cout << "Enter the number of the Fibonacci sequence element: ";
+	std::cin >> n;
+	for (int i = 2; i <= n; ++i)
+	{
+		int fib_n = a + b;
+		a = b;
+		b = fib_n;
+		std::cout << fib_n <<" ";
+	}
 	std::cout << std::endl;
 	std::cout << "Enter the factorial: ";
 	std::cin >> n;
