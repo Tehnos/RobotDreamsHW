0661bccdf273ffbce35ef1f3b3a9bb5f8abbd12e
Tehnos <tehnos2013@gmail.com>
2024-12-17 23:03:44 +0200

    task2

--- HW6/HW6.cpp
+++ HW6/HW6.cpp
@@ -4,17 +4,51 @@ int main()
 {
 	int a1, d, n;
 
-	std::cout << "Enter first element(a1):\n";
-	std::cin >> a1;	
-	std::cout << "Enter arithmetic progression step(d):\n";
+	//std::cout << "Enter first element(a1):\n";
+	//std::cin >> a1;	
+	//std::cout << "Enter arithmetic progression step(d):\n";
+	//std::cin >> d;
+	//std::cout << "Enter the number of the last element of the arithmetic progression(n):\n";
+	//std::cin >> n;
+	//std::cout << "Your arimetic progression with:\n-initial element " << a1 << "\n-progression step " << d << "\n-number of elements " << n<<std::endl << std::endl;
+	//for (int i = 0; i < n; i++)
+	//{
+	//	int element = a1 + i * d;
+	//	std::cout << element << " ";
+	//}
+	//std::cout << std::endl;
+
+
+	std::cout << "Enter weight:\n";
+	std::cin >> a1;
+	std::cout << "Enter height:\n";
 	std::cin >> d;
-	std::cout << "Enter the number of the last element of the arithmetic progression(n):\n";
-	std::cin >> n;
-	std::cout << "Your arimetic progression with:\n-initial element " << a1 << "\n-progression step " << d << "\n-number of elements " << n<<std::endl << std::endl;
-	for (int i = 0; i < n; i++)
+
+	for (int i = 0; i < a1; i++)
 	{
-		int element = a1 + i * d;
-		std::cout << element << " ";
+		for (int j = i; j < d; j++)
+		{
+			std::cout << " * ";
+		}
+		std::cout << std::endl;
 	}
+		std::cout << std::endl;
+	for (int i = 0; i < a1; i++)
+	{
+		for (int j = 0; j <= i; j++)
+		{
+			std::cout << " * ";
+		}
+		std::cout << std::endl;
+	}		
 	std::cout << std::endl;
+	for (int i = 0; i < a1; i++)
+	{
+		for (int j = 0; j < d; j++)
+		{
+			std::cout << " * ";
+		}
+		std::cout << std::endl;
+	}
+
 }
